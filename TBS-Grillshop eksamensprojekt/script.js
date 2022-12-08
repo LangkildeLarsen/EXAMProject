20d10cf0eea1e505afd1bf3859f21c12498dee06
Ester-Helth <118178505+Ester-Helth@users.noreply.github.com>
2022-12-09 00:13:32 +0100

    jeje

--- TBS-Grillshop eksamensprojekt/script.js
+++ TBS-Grillshop eksamensprojekt/script.js
@@ -5,3 +5,10 @@ hamburger. addEventListener ("click", () => {
     hamburger.classList.toggle("active")
     navbar.classList.toggle("active")
 })
+
+document.querySelectorAll ("a").forEach (n=> n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active")
+        navbar.classList.remove("active")
+
+    }))
